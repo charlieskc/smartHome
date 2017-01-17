d5751dcec22abf7b72fdbd9ac55775fb4ca992f5
Siu Kai Ching, Charlie <charlieskc@gmail.com>
2017-01-17 13:11:56 +0800

    init

--- restful.py
+++ restful.py
@@ -16,7 +16,7 @@ tasks = [
         'done': False
     },
     {
-        'id': 2,
+        'id': 2, 
         'title': u'Learn Python',
         'description': u'Need to find a good Python tutorial on the web', 
         'done': False
