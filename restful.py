a59f467eb554056836f134fe29b978a0c50ded10
Siu Kai Ching, Charlie <charlieskc@gmail.com>
2017-02-04 01:05:16 +0800

    Add Dyson hot+cool link and KEF EGG Speaker infrared control as RESTFUL service

--- restful.py
+++ restful.py
@@ -1,60 +1,94 @@
 #!flask/bin/python
 from datetime import datetime
-from flask import Flask, jsonify, abort, make_response, request
 
-from sensehat import demo
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import subprocess
 import sensehat
 
+
 app = Flask(__name__)
+api = Api(app)
 
-tasks = [
-    {
-        'id': 1, 
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
+
+actions = {
+    'action1': {
+        '_id': 1, 
+        'action': 'sensehat',
+        'time': datetime.now().strftime('%H:%M:%S'), 
+        'result': True
     },
-    {
+    'action2':{
         'id': 2, 
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
+        'action': 'IR',
+        'time':datetime.now().strftime('%H:%M:%S'),
+        'result': True
     }
-]
+}
 
-@app.route('/restful/demo', methods=['GET'])
-def demo():
-    sensehat.demo()
+def abort_if_action_not_exist(actionId):
+    if actionId not in actions:
+        abort(404, message="Action {} doesn't exist".format(actionId))
+
+parser = reqparse.RequestParser()
+parser.add_argument('action')
+parser.add_argument('actiontype')
+
+class Action(Resource):
+    def get(self, actionId):
+        abort_if_action_not_exist(actionId)
+        return actions[actionId]
+
+    def delete(self, actionId):
+        abort_if_action_not_exist(actionId)
+        del actions[actionId]
+        return '', 204
+
+    def put(self, actionId):
+        args = parser.parse_args()
+        action = {'action': args['action']}
+        actions[actionId] = action
+        return action, 201
     
-@app.route('/restful', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
 
-@app.route('/restful/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
+class ActionList(Resource):
+    def get(self):
+        return actions
 
-@app.route('/restful', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    def post(self):
+        args = parser.parse_args()
+        actionId = int(max(actions.keys()).lstrip('action')) + 1
+        actionId = 'action%i' % actionId
+        actions[actionId] = {'action': args['action']}
+        return actions[actionId], 201
 
 
+class Perform(Resource):
+    def get(self):
+        args = parser.parse_args()
+        if args['action'] == "sensehat":   
+            return sensehat.demo()
+        elif args['action'] == "kef": 
+            if args['actiontype']:
+                subprocess.check_output(["irsend SEND_ONCE kef " + args['actiontype']], shell=True)
+                return args['actiontype'] + " is triggered."
+        elif args['action'] == "dyson": 
+            if args['actiontype']:
+                subprocess.check_output(["irsend SEND_ONCE dyson " + args['actiontype']], shell=True)
+                return args['actiontype'] + " is triggered."
+        else:
+             abort(404, message="Action is not recognized - Usage example= http://pi.kaiching.net:8080/perform?action=dyson&actiontype=KEY_POWER") 
+    def post(self):
+        args = parser.parse_args()
+        if args['action'] == "sensehat":            
+            return sensehat.demo()
+        abort(404, message="Action is not recognized") 
+
+
+
+api.add_resource(ActionList, '/actionlist')
+api.add_resource(Action, '/action/<actionId>')
+api.add_resource(Perform, '/perform')
 
 if __name__ == '__main__':
-    app.run(debug=True , host='0.0.0.0')
+    app.run(debug=True , host='0.0.0.0', port=8080)
